4b56068534d28a496b3f3939876ebcf0ac417a15
Renukabhavsar <145988235+Renukabhavsar@users.noreply.github.com>
2023-09-25 17:42:00 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,16 @@
+print("Hello, World")
+print(23+2)
+a=2
+b=4
+print(a+b)
+
+x = int(input("enter first number:\n"))
+y = int(input("enter second number:\n"))
+sum = sum([x,y])
+print("sum of", x, "and", y, "is", sum)
+
+num = 10
+if num % 2 == 0:
+print("Entered Number is Even")
+else:
+print("Entered Number is Odd")
